b48f0515a49efbbc2e8ead64ddbbeaba3bca3d9d
Asif Nassar <klmktu.asif@gmail.com>
2024-07-05 20:22:18 +0530

    02-03 - Init

---
+++ 02_spring-boot-spring-core/03_setter-injection/src/main/java/dev/asif/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,20 @@
+package dev.asif.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+@SpringBootApplication(
+		scanBasePackages = {"dev.asif.springcoredemo",
+							"dev.asif.util"}
+)
+*/
+
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
---
+++ 02_spring-boot-spring-core/03_setter-injection/src/main/java/dev/asif/springcoredemo/common/Coach.java
@@ -0,0 +1,7 @@
+package dev.asif.springcoredemo.common;
+
+public interface Coach {
+
+    String getDailyWorkout();
+
+}
