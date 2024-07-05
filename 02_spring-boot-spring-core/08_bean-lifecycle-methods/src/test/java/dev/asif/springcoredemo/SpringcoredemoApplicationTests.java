bf38c851b60b14c994be97d12b065487ad6a5622
Asif Nassar <klmktu.asif@gmail.com>
2024-07-06 02:41:51 +0530

    02-08 - Init

---
+++ 02_spring-boot-spring-core/08_bean-lifecycle-methods/src/test/java/dev/asif/springcoredemo/SpringcoredemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.asif.springcoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcoredemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
