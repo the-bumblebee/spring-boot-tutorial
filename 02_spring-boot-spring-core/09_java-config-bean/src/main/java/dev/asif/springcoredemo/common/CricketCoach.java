9fa517fb35b643c1c947961dd0f492eb49da81a8
Asif Nassar <klmktu.asif@gmail.com>
2024-07-06 02:55:49 +0530

    02-09 - No Component annotation for SwimCoach class

--- 02_spring-boot-spring-core/09_java-config-bean/src/main/java/dev/asif/springcoredemo/common/CricketCoach.java
+++ 02_spring-boot-spring-core/09_java-config-bean/src/main/java/dev/asif/springcoredemo/common/CricketCoach.java
@@ -1,9 +1,5 @@
 package dev.asif.springcoredemo.common;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,18 +7,6 @@ public class CricketCoach implements Coach {
 
     public CricketCoach() {
         System.out.println("In constructor: " + getClass().getSimpleName());
-    }
-
-    // Define Init method (Post Constructor)
-    @PostConstruct
-    public void doMyStartupStuff() {
-        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
-    }
-
-    // Define Destroy method (Before Deconstruction)
-    @PreDestroy
-    public void doMyCleanupStuff() {
-        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
     }
 
     @Override
