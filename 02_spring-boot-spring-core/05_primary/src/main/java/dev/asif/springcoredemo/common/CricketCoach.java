01c98c09fe50d7c9a9c0fcfd4733d55c32cce6bd
Asif Nassar <klmktu.asif@gmail.com>
2024-07-05 20:50:37 +0530

    02-05 - Multiple Primary annotations

--- 02_spring-boot-spring-core/05_primary/src/main/java/dev/asif/springcoredemo/common/CricketCoach.java
+++ 02_spring-boot-spring-core/05_primary/src/main/java/dev/asif/springcoredemo/common/CricketCoach.java
@@ -1,8 +1,11 @@
 package dev.asif.springcoredemo.common;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
+// Multiple @Primary would cause Spring to throw error
 @Component
+@Primary
 public class CricketCoach implements Coach {
 
     @Override
