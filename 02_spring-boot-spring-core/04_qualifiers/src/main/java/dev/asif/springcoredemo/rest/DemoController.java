5222b2d8b65852fd537dc3561ee54731afe25f6d
Asif Nassar <klmktu.asif@gmail.com>
2024-07-05 20:32:37 +0530

    02-04 - Init

---
+++ 02_spring-boot-spring-core/04_qualifiers/src/main/java/dev/asif/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package dev.asif.springcoredemo.rest;
+
+import dev.asif.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //  Define private field for dependency
+    private Coach myCoach;
+
+    @Autowired
+    public void setCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
