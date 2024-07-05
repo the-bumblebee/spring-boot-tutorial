86c9e1957e8096620cf7456544a1ad0459d41048
Asif Nassar <klmktu.asif@gmail.com>
2024-07-06 02:52:55 +0530

    02-09 - Init

---
+++ 02_spring-boot-spring-core/09_java-config-bean/src/main/java/dev/asif/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,33 @@
+package dev.asif.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    // Define Init method (Post Constructor)
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    // Define Destroy method (Before Deconstruction)
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff(): " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+
+}
---
+++ 02_spring-boot-spring-core/09_java-config-bean/src/main/java/dev/asif/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package dev.asif.springcoredemo.rest;
+
+import dev.asif.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //  Define private field for dependency
+    private Coach myCoach;
+
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
