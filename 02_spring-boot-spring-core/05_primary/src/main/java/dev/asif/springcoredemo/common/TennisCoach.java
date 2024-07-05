9fee2242b57f90f716e3775c255da6448474104a
Asif Nassar <klmktu.asif@gmail.com>
2024-07-05 20:46:03 +0530

    02-05 - Init

---
+++ 02_spring-boot-spring-core/05_primary/src/main/java/dev/asif/springcoredemo/common/TennisCoach.java
@@ -0,0 +1,13 @@
+package dev.asif.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice your backhand volley";
+    }
+
+}
---
+++ 02_spring-boot-spring-core/05_primary/src/main/java/dev/asif/springcoredemo/rest/DemoController.java
@@ -0,0 +1,27 @@
+package dev.asif.springcoredemo.rest;
+
+import dev.asif.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    //  Define private field for dependency
+    private Coach myCoach;
+
+    // @Qualifier used to specify which implementation to inject
+    // Bean id - Name of the class, with first letter lower case
+    @Autowired
+    public DemoController(@Qualifier("cricketCoach") Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+}
