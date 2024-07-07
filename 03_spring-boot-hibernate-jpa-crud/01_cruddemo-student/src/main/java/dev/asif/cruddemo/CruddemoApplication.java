07e1cdc2dd925df1745bc0b98bb21c5bd4958002
Asif Nassar <klmktu.asif@gmail.com>
2024-07-07 15:34:51 +0530

    03-01 - Updated code to create and save Student in db

--- 03_spring-boot-hibernate-jpa-crud/01_cruddemo-student/src/main/java/dev/asif/cruddemo/CruddemoApplication.java
+++ 03_spring-boot-hibernate-jpa-crud/01_cruddemo-student/src/main/java/dev/asif/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package dev.asif.cruddemo;
 
+import dev.asif.cruddemo.dao.StudentDAO;
+import dev.asif.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +15,28 @@ public class CruddemoApplication {
 	}
 
 	// Bean for creating command line application
+	// Inject studentDAO. Give reference and spring will inject accordingly.
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// Create the Student object
+		System.out.println("Creating new Student object...");
+		Student tempStudent = new Student("Paul", "Doe", "pauldoe@xyz.com");
+
+		// Save the Student object
+		System.out.println("Saving the Student...");
+		studentDAO.save(tempStudent);
+
+		// Display ID of saved Student object
+		System.out.println("Saved Student. Generated ID: " + tempStudent.getId());
 
 	}
 
