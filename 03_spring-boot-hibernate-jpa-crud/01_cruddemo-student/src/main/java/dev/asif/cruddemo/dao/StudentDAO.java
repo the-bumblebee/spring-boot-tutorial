413ba805f8678584fa1109dea11f02cc45e27950
Asif Nassar <klmktu.asif@gmail.com>
2024-07-07 15:27:07 +0530

    03-01 - Added DAO and DAOImpl classes

---
+++ 03_spring-boot-hibernate-jpa-crud/01_cruddemo-student/src/main/java/dev/asif/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,9 @@
+package dev.asif.cruddemo.dao;
+
+import dev.asif.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+
+}
---
+++ 03_spring-boot-hibernate-jpa-crud/01_cruddemo-student/src/main/java/dev/asif/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,31 @@
+package dev.asif.cruddemo.dao;
+
+import dev.asif.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+// Supports component scanning
+// Translates JDBC exceptions
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    // Define field for entity manager
+    private EntityManager entityManager;
+
+    //Inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    // Implement save method
+    // @Transactional annotation since doing an update on database
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+
+}
