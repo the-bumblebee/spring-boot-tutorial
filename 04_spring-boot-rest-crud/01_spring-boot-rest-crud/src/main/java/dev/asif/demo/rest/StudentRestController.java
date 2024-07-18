e530f9f866645760759cd1af4c910bed54e1226c
Asif Nassar <klmktu.asif@gmail.com>
2024-07-19 03:07:27 +0530

    04-01 - Added excpetion handling

--- 04_spring-boot-rest-crud/01_spring-boot-rest-crud/src/main/java/dev/asif/demo/rest/StudentRestController.java
+++ 04_spring-boot-rest-crud/01_spring-boot-rest-crud/src/main/java/dev/asif/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package dev.asif.demo.rest;
 
 import dev.asif.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,6 +39,26 @@ public class StudentRestController {
     // Define endpoint "/students/{studentId}" - Return student at index
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+
+        // Check the studentId against list size
+        if ( (studentId >= students.size()) || (studentId < 0) ) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
         return students.get(studentId);
+    }
+
+    // Add an exception handler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+
+        // Create a StudentErrorResponse
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(exc.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        // Return ResponseEntity
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }
