3103c4425fd0bab5014d14b9c57e0543c123e466
Asif Nassar <klmktu.asif@gmail.com>
2024-07-22 20:20:48 +0530

    04-03 - Added to use JpaRepository instead of DAO (Entity Manager)

--- 04_spring-boot-rest-crud/03_spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/dev/asif/cruddemo/service/EmployeeService.java
+++ 04_spring-boot-rest-crud/03_spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/dev/asif/cruddemo/service/EmployeeService.java
@@ -1,6 +1,5 @@
 package dev.asif.cruddemo.service;
 
-import dev.asif.cruddemo.dao.EmployeeDAO;
 import dev.asif.cruddemo.entity.Employee;
 
 import java.util.List;
--- 04_spring-boot-rest-crud/03_spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/dev/asif/cruddemo/service/EmployeeServiceImpl.java
+++ 04_spring-boot-rest-crud/03_spring-boot-rest-crud-employee-with-spring-data-jpa/src/main/java/dev/asif/cruddemo/service/EmployeeServiceImpl.java
@@ -1,43 +1,55 @@
 package dev.asif.cruddemo.service;
 
-import dev.asif.cruddemo.dao.EmployeeDAO;
+import dev.asif.cruddemo.dao.EmployeeRepository;
 import dev.asif.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     // Constructor injection to inject EmployeeDAO
     @Autowired
-    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int id) {
-        return  employeeDAO.findById(id);
+        Optional<Employee> result = employeeRepository.findById(id);
+
+        Employee employee = null;
+
+        if (result.isPresent()) {
+            employee = result.get();
+        }
+        else {
+            throw new RuntimeException("Employee id not find - " + id);
+        }
+
+        return employee;
     }
 
-    @Transactional
+    // @Transactional - Can be removed as JpaRepository provides this functionality
     @Override
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
-    @Transactional
+    // @Transactional - Can be removed as JpaRepository provides this functionality
     @Override
     public void deleteById(int id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
