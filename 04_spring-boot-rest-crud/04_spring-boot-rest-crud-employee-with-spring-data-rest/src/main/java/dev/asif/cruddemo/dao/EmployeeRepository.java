9feed831e905aa6edd9008ad610bbb875fe91599
Asif Nassar <klmktu.asif@gmail.com>
2024-08-26 18:25:02 +0530

    04-04 - Changed EmployeeRepository resource path

--- 04_spring-boot-rest-crud/04_spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/dev/asif/cruddemo/dao/EmployeeRepository.java
+++ 04_spring-boot-rest-crud/04_spring-boot-rest-crud-employee-with-spring-data-rest/src/main/java/dev/asif/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package dev.asif.cruddemo.dao;
 
 import dev.asif.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // No need to write any code
